bf9e77dc3f8c0a93d2a807a8a15a327b2b3a1149
besarbertasholli <besarbertasholli@hotmail.com>
2022-06-12 23:44:12 +0200

    Update deprecated method

--- wagtailimportexport/wagtail_hooks.py
+++ wagtailimportexport/wagtail_hooks.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from wagtailimportexport import admin_urls
 from wagtailimportexport.compat import hooks, MenuItem
