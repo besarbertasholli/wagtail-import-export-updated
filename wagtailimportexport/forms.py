f76afd76a863a2e99bb95f37aaf7b92cf9b1da11
besarbertasholli <besarbertasholli@hotmail.com>
2022-06-12 23:42:43 +0200

    Update deprecated method

--- wagtailimportexport/forms.py
+++ wagtailimportexport/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 
 from wagtailimportexport.compat import AdminPageChooser, Page, WAGTAIL_VERSION_2_OR_GREATER
 
